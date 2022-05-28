4db6659a492bd8db1c9a503a0c4d300fd2eb23f6
Brijesh Rohit <brijeshrohit1.7@gmail.com>
2022-05-28 22:04:24 +0530

    Counter-App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import UseReducer from "./component/Hooks/useReducer";
+
+const App = () => {
+  return (
+    <>
+      <UseReducer />
+    </>
+  );
+};
+
+export default App;
